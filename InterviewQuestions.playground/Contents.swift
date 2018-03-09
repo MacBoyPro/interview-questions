595eaa5f598e7d67dbf174ce7aef71165cc21049
Lee Lundrigan <lee.lundrigan@gmail.com>
2018-03-08 22:02:06 -0800

    added link list

--- InterviewQuestions.playground/Contents.swift
+++ InterviewQuestions.playground/Contents.swift
@@ -2,8 +2,53 @@
 
 import UIKit
 
-var str = "Hello, playground"
+print("--------- WELCOME TO CODING INTERVIEW PALOOZA ---------")
 
-print(str)
+print("Q: Create a linked list")
+print("INPUT :  [30,25,20,15,10,5]")
+print("OUTPUT:  ->30->25->20->15->10->5")
+
+var inputValues: [Int] = [30,25,20,15,10,5]
+
+class Node {
+    var leftNode: Node?
+    var rightNode: Node?
+    var value: Int
+    
+    init(left: Node?, right: Node?, value: Int) {
+        self.leftNode = left
+        self.rightNode = right
+        self.value = value
+    }
+}
+
+var value: Int = inputValues.popLast()!
+var lastNode: Node?
+while inputValues.count > 0 {
+    value = inputValues.popLast()!
+    
+    let nextNode: Node = Node(left: nil, right: lastNode, value: value)
+    
+    if(lastNode != nil) {
+        lastNode!.leftNode = nextNode
+    }
+    
+    lastNode = nextNode
+}
+
+let head: Node = lastNode!
+
+var nextNode: Node? = head
+while nextNode != nil {
+    let terminator: String = nextNode!.rightNode != nil ? "->" : ""
+    print(nextNode!.value, terminator: terminator)
+    
+    nextNode = nextNode!.rightNode
+}
+
+print("")
+print("Q: Reverse a linked list")
+print("INPUT : ->30->25->20->15->10->5")
+print("OUTPUT: ->5->10->15->20->25->30")
 
 
